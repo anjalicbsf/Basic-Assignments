6a73731f6a46d6d4d262c16cb20a00be5ed46eee
anjali <anjalic@bsf.io>
2017-03-02 15:43:02 +0530

    flip is designed with plugin

--- Popup-assignment-2/js/popup.js
+++ Popup-assignment-2/js/popup.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
       $(".info-up").addClass("open");  
   });
 
+
+  // $("#logo-right").click(function(){
+  // 	 $(".info-right").removeClass("close"); 
+  //     $(".info-right").addClass("open");  
+  // });
+  // $("#logo-bottom").click(function(){
+  // 	 $(".info-bottom").removeClass("close"); 
+  //     $(".info-bottom").addClass("open");  
+  // });
+  // $("#logo-lef").click(function(){
+  // 	 $(".info-lef").removeClass("close"); 
+  //     $(".info-lef").addClass("open");  
+  // });
+
 });
