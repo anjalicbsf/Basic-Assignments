dc18ff8804d976455e16556b6e3c96bbd6b5b9e1
anjali <anjalic@bsf.io>
2017-03-02 16:42:27 +0530

    all info for all logo is displaying successfully

--- Popup-assignment-2/js/popup.js
+++ Popup-assignment-2/js/popup.js
@@ -1,22 +1,10 @@
 $(document).ready(function(){
 
-  $("#logo-up").hover(function(){
-  	 $(".info-up").removeClass("close"); 
-      $(".info-up").addClass("open");  
+  $(".logo").hover(function(){
+    var direction = $( this ).data('direction');
+    console.log(direction);
+    console.log($( '.info-' + direction ));
+    $( '.circle-info' ).children().removeClass( 'open' );
+    $( '.info-' + direction ).addClass( 'open' );
   });
-
-
-  // $("#logo-right").click(function(){
-  // 	 $(".info-right").removeClass("close"); 
-  //     $(".info-right").addClass("open");  
-  // });
-  // $("#logo-bottom").click(function(){
-  // 	 $(".info-bottom").removeClass("close"); 
-  //     $(".info-bottom").addClass("open");  
-  // });
-  // $("#logo-lef").click(function(){
-  // 	 $(".info-lef").removeClass("close"); 
-  //     $(".info-lef").addClass("open");  
-  // });
-
 });
