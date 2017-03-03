594a66a65a8371f1fb6ff6ecfdb5e9d012b28307
anjali <anjalic@bsf.io>
2017-03-03 09:50:26 +0530

    flip property for simple box working,interactive banner working

--- interactive-banner-assgn/assests/js/banner.js
+++ interactive-banner-assgn/assests/js/banner.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   $('.img-base').hover(function(){
 			$('.panel').addClass('open');
+			$('.img-parent').addClass('open');
 		},function(){
 			$('.panel').removeClass('open');
+			$('.img-parent').removeClass('open');
 		});
 })
+
